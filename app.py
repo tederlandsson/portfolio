f4d0e2279cefb995869f78fd7cdce7c20be11c61
bratislavablue <bratislavablue@users.noreply.github.com>
2020-10-17 15:58:17 +0200

    add new pages

--- app.py
+++ app.py
@@ -1,23 +1,26 @@
 from flask import Flask, render_template
+import content
 app = Flask(__name__)
+
 
 @app.route('/')
 def home():
   """Landing page'"""
   return render_template('home.html',
-    title='Demo site',
-    description='This is a test description.'
+    content=content.homepage
   )
-
-@app.route('/hello/<name>')
-def hello(name):
-  return render_template('home.html', name=name)
 
 @app.route('/about')
 def about():
+  """About page"""
   return render_template('about.html', 
-    about_title='About me',
-    about_h1='About header'
+    content=content.about
+  )
+
+@app.route('/contact')
+def contact():
+  """Contact page"""
+  return render_template('contact.html',  content=content.contact
   )
 
 
