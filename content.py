18bb76e28effc1ee41c7bf215f7cb326563901ce
bratislavablue <bratislavablue@users.noreply.github.com>
2020-10-23 18:46:58 +0200

    update content

---
+++ content.py
@@ -0,0 +1,64 @@
+homepage = {
+  'title': 'Demo site',
+  'h1': 'Demo site',
+  'description': 'This is a test description from a dictionary',
+  'text': '| "American whole magazine truth stop whose. On traditional measure example sense peace. Would mout relate own chair. Together range line beyond. First policy daughter need kind miss."'
+}
+
+
+about = {
+  'title': 'About me',
+  'h1':'About me',
+  'description': 'Experience',
+  'programming': ['Javascript', 'Python', 'Node.js', 'Selenium webdriver', 'Bash', 'Auto Hotkey', 'SQL'],
+  'software': ['WebdriverIO', 'Selenium webdriver', 'Cucumber', 'JMeter', 'Git', 'BrowserStack', 'Azure DevOps', 'Postman', 'Sitecore'],
+  'experience': [
+    {
+        'company': 'Volvo Car Corporation',
+        'started': 'Jun. 2019',
+        'ended': 'Present',
+        'role': 'Quality engineer',
+        'responsibilities': [
+          "Using the Node.js framework WebdriverIO to automate web pages consisting of React components",
+          "Responsible quality engineer in a product that builds some of the company's most heavily trafficed web pages",
+          "Implementing automated tests end-to-end.",
+          "Quality assurance on new pages and market rollouts"
+          ],
+    },
+    {
+      'company': 'Acando Consulting',
+      'started': 'Aug. 2017',
+      'ended': 'Jun. 2019',
+      'role': 'Software test lead',
+      'responsibilities': [
+        'Test leading e-commerce projects end-to-end',
+        'Designing test cases',
+        'Executing tests of web applications and REST APIs to ensure functionality and integration end-to-end'
+        ]
+      },
+    {
+      'company': 'SCAN COIN',
+      'started': 'Jan. 2016',
+      'ended': 'Jul. 2017',
+      'role': 'Quality assurance manager',
+      'responsibilities': [
+        'Quality assurance for cash management software',
+        'Owner of internal test environments',
+        'Developing test strategies and test cases'
+        ]
+    },
+  ],
+  'education': {
+    'school': 'Lund university',
+    'started': '2011',
+    'ended': '2014',
+    'major': 'Informatics',
+    'level': 'Bachelors',
+  }
+}
+
+contact = {
+  'title': 'Contact me',
+  'h1': 'Get in touch',
+  'description': "Let's talk"
+}
