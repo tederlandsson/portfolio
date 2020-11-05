f2fa7ef790eb33b380a768059986ed8ab7eded0e
ted <ted.erlandsson@volvocars.com>
2020-11-05 22:36:43 +0100

    add navbar

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
+import Navigation from '../navigation/navigation';
 
 export default class Navheader extends Component {
   render(props) {
     return (
-        <div className="border-b font-bold p-3"> 
-          <h1>{this.props.content.aboutMe.name}</h1>
-      </div>
+        <header className="border-b font-bold p-3 flex justify-between items-center">
+          <h1>
+            {this.props.content.aboutMe.name}
+          </h1>
+          <Navigation />
+      </header>
     );
   }
 }
