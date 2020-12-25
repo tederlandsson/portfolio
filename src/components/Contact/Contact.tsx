6c8b3415d11782c4917b8ef97ce1c069ad75acf1
bratislavablue <bratislavablue@users.noreply.github.com>
2020-12-25 18:50:57 +0100

    refactor contact to be functional component

--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import StyledContact from './Contact.styled';
 import { ContactInfo } from '../../config/about';
 
@@ -6,36 +6,32 @@ import { Icon } from '@iconify/react';
 import githubFilled from '@iconify-icons/ant-design/github-filled';
 import linkedinFilled from '@iconify-icons/ant-design/linkedin-filled';
 
-
-export default class Contact extends Component {
-
-  render() {
-  
-    return (
-      <StyledContact>
-        <div className="contact">
-          <h2>Contact</h2>
-          <ul className="no-bullets">
-            <p>Let's get in touch. Here's how to find me</p>
-            <li>
-              Drop me an email at <br />{ContactInfo.email}
-            </li>
-            <br />
-            <li>
-              Find me on LinkedIn: <br /> <a href="https://www.linkedin.com/in/tederlandsson">{ContactInfo.linkedin}</a>
-              <Icon icon={linkedinFilled} />
-            </li>
-            
-            <br />
-            <li>
-              Find me on Github:<br /> <a href="https://www.github.com">{ContactInfo.github}</a>
-              <Icon icon={githubFilled}/>
-            </li>
-            
-          </ul>  
-        </div>
-      </StyledContact>
-    );
-  }
-
+const Contact: React.FC = () => {
+  return (
+    <StyledContact>
+      <div className="contact">
+        <h2>Contact</h2>
+        <ul className="no-bullets">
+          <p>Let's get in touch. Here's how to find me</p>
+          <li>
+            Drop me an email at <br />{ContactInfo.email}
+          </li>
+          <br />
+          <li>
+            Find me on LinkedIn: <br /> <a href="https://www.linkedin.com/in/tederlandsson">{ContactInfo.linkedin}</a>
+            <Icon icon={linkedinFilled} />
+          </li>
+          
+          <br />
+          <li>
+            Find me on Github:<br /> <a href="https://www.github.com">{ContactInfo.github}</a>
+            <Icon icon={githubFilled}/>
+          </li>
+          
+        </ul>  
+      </div>
+    </StyledContact>
+  );
 }
+  
+export default Contact;
