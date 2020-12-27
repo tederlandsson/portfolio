70e9d11b0721767840ff83bf299bbc2d2d4e36ec
bratislavablue <bratislavablue@users.noreply.github.com>
2020-12-27 12:00:39 +0100

    refactoring by adding in utility functions for cypress tests

---
+++ utils/ui_utils/utilityFunctions.ts
@@ -0,0 +1,48 @@
+class UtilityFunctions {
+  verbose: boolean;
+
+  constructor() {
+    this.verbose = false;
+  }
+
+  /**
+  * Returns a cypress selector
+  * @param selector
+  */
+  getElement(selector: string) {
+    return cy.get(selector);
+  }
+
+  /**
+   * Checks that an element is displayed
+   * @param {Cypress.Chainable<any>} element 
+   */
+  verifyElementIsDisplayed(element: Cypress.Chainable<any>): void {
+    element.scrollIntoView();
+    element.should('be.visible');
+  }
+
+  /**
+   * Get text from cypresse element
+   * @param element
+   */
+  getTextFromElement(element: Cypress.Chainable<HTMLElement>): Cypress.Chainable<any> {
+    if (this.verbose) {
+      const text = element.invoke('text');
+      console.log(`Element text:\t${text}`)
+    }
+    return element.invoke('text');
+  }
+
+  /**
+   * Clicks a cypress selector
+   * @param element
+   */
+  clickElement(element: Cypress.Chainable<any>): void {
+    element.scrollIntoView();
+    element.click();
+    this.verbose && console.log(`Clicked element ${element}`);
+  }
+}
+
+export default new UtilityFunctions();
